71db87432c628af04ebcdf19d31797778eafdcf2
tula3and <tula3and@gmail.com>
2022-02-08 17:39:19 +0900

    Add questions and print on the page

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 import streamlit as st
 from qrng import random_number
+import csv
+import time
 
-st.write("""
-        # Random Question Generator
-        질문을 추천해드려요!
-        """)
+# set page title and icon
+st.set_page_config(page_title="Random Questions", page_icon="🐈", layout="centered")
+st.title("🐈 Random Questions!")
+st.caption("You know what? Random numbers used in this page are from Hadamard gates in Qiskit!")
 
+# welcome balloons
+st.balloons()
+
+# set a default language option
+option = st.selectbox(
+        'Choose language first',
+        ('한국어', 'English'))
+
+if option == '한국어':
+    language = 0
+else:
+    language = 1
+
+# choose one number from 0 to 31
 num = random_number()
 
-st.write(num)
+# read questions for a csv file
+f = open('questions.csv', 'r', encoding="UTF-8")
+rdr = csv.reader(f)
 
+# count the number of questions
+cnt = -1
+questions = []
+for line in rdr:
+    cnt += 1
+    if (cnt == 0): continue
+    temp = []
+    temp.append(line[0])
+    temp.append(line[1])
+    questions.append(temp)
+
+f.close()
+
+# pick another number
+if st.button('New Question!'):
+    num = random_number()
+
+with st.spinner('Cats is running around...'):
+    time.sleep(1)
+
+# print selected question
+st.success(questions[num % cnt][language])
